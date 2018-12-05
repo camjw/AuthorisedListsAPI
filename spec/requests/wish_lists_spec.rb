0e83828ce4f3f33cabe68f7cc8143aa1b7521672
camjw <camjw119@gmail.com>
2018-12-05 21:15:32 +0000

    Updating wish list spec

--- spec/requests/wish_lists_spec.rb
+++ spec/requests/wish_lists_spec.rb
@@ -1,107 +1,125 @@
-# frozen_string_literal: true
-
+# spec/requests/wish_lists_spec.rb
 require 'rails_helper'
 
-RSpec.describe 'wish_lists API', type: :request do
-  # initialize test data
-  let!(:wish_lists) { create_list(:wish_list, 10) }
-  let(:wish_list_id) { wish_lists.first.id }
+RSpec.describe 'WishLists API' do
+  # Initialize the test data
+  let!(:user) { create(:user) }
+  let!(:wish_lists) { create_list(:wish_list, 20, user_id: user.id) }
+  let(:user_id) { user.id }
+  let(:id) { wish_lists.first.id }
 
-  # Test suite for GET /wish_lists
-  describe 'GET /wish_lists' do
-    # make HTTP get request before each example
-    before { get '/wish_lists' }
+  # Test suite for GET /users/:user_id/wish_lists
+  describe 'GET /users/:user_id/wish_lists' do
+    before { get "/users/#{user_id}/wish_lists" }
 
-    it 'returns wish_lists' do
-      # Note `json` is a custom helper to parse JSON responses
-      expect(json).not_to be_empty
-      expect(json.size).to eq(10)
-    end
-
-    it 'returns status code 200' do
-      expect(response).to have_http_status(200)
-    end
-  end
-
-  # Test suite for GET /wish_lists/:id
-  describe 'GET /wish_lists/:id' do
-    before { get "/wish_lists/#{wish_list_id}" }
-
-    context 'when the record exists' do
-      it 'returns the wish_list' do
-        expect(json).not_to be_empty
-        expect(json['id']).to eq(wish_list_id)
-      end
-
+    context 'when user exists' do
       it 'returns status code 200' do
         expect(response).to have_http_status(200)
       end
+
+      it 'returns all user wish_lists' do
+        expect(json.size).to eq(20)
+      end
     end
 
-    context 'when the record does not exist' do
-      let(:wish_list_id) { 100 }
+    context 'when user does not exist' do
+      let(:user_id) { 0 }
 
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
       end
 
       it 'returns a not found message' do
-        expect(response.body).to include("Couldn't find WishList")
+        expect(response.body).to match(/Couldn't find User/)
       end
     end
   end
 
-  # Test suite for POST /wish_lists
-  describe 'POST /wish_lists' do
-    # valid payload
-    let(:valid_attributes) { { name: 'Learn Elm', viewable: true } }
+  # Test suite for GET /users/:user_id/wish_lists/:id
+  describe 'GET /users/:user_id/wish_lists/:id' do
+    before { get "/users/#{user_id}/wish_lists/#{id}" }
 
-    context 'when the request is valid' do
-      before { post '/wish_lists', params: valid_attributes }
-
-      it 'creates a wish_list' do
-        expect(json['name']).to eq('Learn Elm')
+    context 'when user wish_list exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
       end
+
+      it 'returns the wish_list' do
+        expect(json['id']).to eq(id)
+      end
+    end
+
+    context 'when user wish_list does not exist' do
+      let(:id) { 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find WishList/)
+      end
+    end
+  end
+
+  # Test suite for PUT /users/:user_id/wish_lists
+  describe 'POST /users/:user_id/wish_lists' do
+    let(:valid_attributes) { { name: 'Stuff', viewable: 0 } }
+
+    context 'when request attributes are valid' do
+      before { post "/users/#{user_id}/wish_lists", params: valid_attributes }
 
       it 'returns status code 201' do
         expect(response).to have_http_status(201)
       end
     end
 
-    context 'when the request is invalid' do
-      before { post '/wish_lists', params: {} }
+    context 'when an invalid request' do
+      before { post "/users/#{user_id}/wish_lists", params: {} }
 
       it 'returns status code 422' do
         expect(response).to have_http_status(422)
       end
 
-      it 'returns a validation failure message' do
-        expect(response.body)
-          .to include("Validation failed: Name can't be blank")
+      it 'returns a failure message' do
+        expect(response.body).to match(/Validation failed: Name can't be blank/)
       end
     end
   end
 
-  # Test suite for PUT /wish_lists/:id
-  describe 'PUT /wish_lists/:id' do
-    let(:valid_attributes) { { name: 'Shopping' } }
+  # Test suite for PUT /users/:user_id/wish_lists/:id
+  describe 'PUT /users/:user_id/wish_lists/:id' do
+    let(:valid_attributes) { { name: 'Mozart' } }
 
-    context 'when the record exists' do
-      before { put "/wish_lists/#{wish_list_id}", params: valid_attributes }
+    before { put "/users/#{user_id}/wish_lists/#{id}", params: valid_attributes }
 
-      it 'updates the record' do
-        expect(response.body).to be_empty
-      end
-
+    context 'when wish_list exists' do
       it 'returns status code 204' do
         expect(response).to have_http_status(204)
       end
+
+      it 'updates the wish_list' do
+        updated_wish_list = WishList.find(id)
+        expect(updated_wish_list.name).to match(/Mozart/)
+      end
+    end
+
+    context 'when the wish_list does not exist' do
+      let(:id) { 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find WishList/)
+      end
     end
   end
 
-  # Test suite for DELETE /wish_lists/:id
-  describe 'DELETE /wish_lists/:id' do
-    before { delete "/wish_lists/#{wish_list_id}" }
+  # Test suite for DELETE /users/:id
+  describe 'DELETE /users/:id' do
+    before { delete "/users/#{user_id}/wish_lists/#{id}" }
 
     it 'returns status code 204' do
       expect(response).to have_http_status(204)
