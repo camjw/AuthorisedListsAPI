ff271f9b2773b28790d8b22350539687314ab031
camjw <camjw119@gmail.com>
2018-12-05 20:45:56 +0000

    Filled out product spec

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,12 @@
+
+require 'spec_helper'
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  it { should have_many(:items) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:url) }
+  it { should validate_presence_of(:producer) }
+  it { should validate_presence_of(:price) }
+end
