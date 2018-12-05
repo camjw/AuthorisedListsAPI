48c6985e7a7f4ccbc6b7212928bfe3f70348f248
camjw <camjw119@gmail.com>
2018-12-05 16:26:22 +0000

    Created routing for users and items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :users do
+    resources :wish_lists do
+      resources :items
+    end
+  end 
   resources :wish_lists do
     resources :items
   end
+  resources :items
 end
