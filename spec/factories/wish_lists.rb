dae8457f9fb5abbbbbc74dd6200c2fd371820fd9
camjw <camjw119@gmail.com>
2018-12-05 15:47:23 +0000

    Completed factories

--- spec/factories/wish_lists.rb
+++ spec/factories/wish_lists.rb
@@ -3,13 +3,6 @@ FactoryBot.define do
     name { Faker::Lorem.word }
     created_by { Faker::Number.number(10) }
     public { false }
-    user_id nil
+    user_id { nil }
   end
 end
-
-t.string 'name'
-t.boolean 'public'
-t.bigint 'user_id'
-t.datetime 'created_at', null: false
-t.datetime 'updated_at', null: false
-t.index ['user_id'], name: 'index_wish_lists_on_user_id'
