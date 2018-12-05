c47d10638ac77abe8380211bc77dd6237fa58ec7
camjw <camjw119@gmail.com>
2018-12-05 20:51:33 +0000

    Updated items factory to add foreign key

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     bought { false }
     wish_list_id { nil }
     original_price { Faker::Number.number(100) }
-    price { Faker::Number.number(100) }
     bid { Faker::Number.number(100) }
+    product_id { nil }
   end
 end
