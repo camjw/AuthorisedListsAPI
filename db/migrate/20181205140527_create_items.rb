be38af48010df2eb48fcc7ce04eca9267725d642
camjw <camjw119@gmail.com>
2018-12-05 14:05:55 +0000

    Updated database

---
+++ db/migrate/20181205140527_create_items.rb
@@ -0,0 +1,14 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.boolean :bought
+      t.float :original_price
+      t.float :price
+      t.float :bid
+      t.references :wish_list, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
