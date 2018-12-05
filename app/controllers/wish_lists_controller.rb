2d724653db89519bc33bc23995d4b6e534c12daf
camjw <camjw119@gmail.com>
2018-12-05 15:57:15 +0000

    Added concerns

--- app/controllers/wish_lists_controller.rb
+++ app/controllers/wish_lists_controller.rb
@@ -1,2 +1,43 @@
 class WishListsController < ApplicationController
+  before_action :set_wish_list, only: [:show, :update, :destroy]
+
+  # GET /wish_lists
+  def index
+    @wish_lists = WishList.all
+    json_response(@wish_lists)
+  end
+
+  # POST /wish_lists
+  def create
+    @wish_list = WishList.create!(wish_list_params)
+    json_response(@wish_list, :created)
+  end
+
+  # GET /wish_lists/:id
+  def show
+    json_response(@wish_list)
+  end
+
+  # PUT /wish_lists/:id
+  def update
+    @wish_list.update(wish_list_params)
+    head :no_content
+  end
+
+  # DELETE /wish_lists/:id
+  def destroy
+    @wish_list.destroy
+    head :no_content
+  end
+
+  private
+
+  def wish_list_params
+    # whitelist params
+    params.permit(:name, :user_id)
+  end
+
+  def set_wish_list
+    @wish_list = WishList.find(params[:id])
+  end
 end
