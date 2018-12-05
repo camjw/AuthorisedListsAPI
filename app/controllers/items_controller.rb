86cc0cc46f7ee0e2db8d4697beb5a2bd85916512
camjw <camjw119@gmail.com>
2018-12-05 21:11:08 +0000

    passing tests

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,32 +1,31 @@
-# frozen_string_literal: true
-
+# app/controllers/items_controller.rb
 class ItemsController < ApplicationController
-  before_action :set_item, only: %i[show update destroy]
+  before_action :set_wish_list
+  before_action :set_wish_list_item, only: [:show, :update, :destroy]
 
-  # GET /items
+  # GET /wish_lists/:wish_list_id/items
   def index
-    @items = Item.all
-    json_response(@items)
+    json_response(@wish_list.items)
   end
 
-  # POST /items
-  def create
-    @item = Item.create!(item_params)
-    json_response(@item, :created)
-  end
-
-  # GET /items/:id
+  # GET /wish_lists/:wish_list_id/items/:id
   def show
     json_response(@item)
   end
 
-  # PUT /items/:id
+  # POST /wish_lists/:wish_list_id/items
+  def create
+    @wish_list.items.create!(item_params)
+    json_response(@wish_list, :created)
+  end
+
+  # PUT /wish_lists/:wish_list_id/items/:id
   def update
     @item.update(item_params)
     head :no_content
   end
 
-  # DELETE /items/:id
+  # DELETE /wish_lists/:wish_list_id/items/:id
   def destroy
     @item.destroy
     head :no_content
@@ -35,11 +34,14 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    # whitelist params
-    params.permit(:name, :bought, :original_price, :bid, :wish_list_id, :product_id)
+    params.permit(:name, :bought, :original_price, :bid)
   end
 
-  def set_item
-    @item = Item.find(params[:id])
+  def set_wish_list
+    @wish_list = WishList.find(params[:wish_list_id])
+  end
+
+  def set_wish_list_item
+    @item = @wish_list.items.find_by!(id: params[:id]) if @wish_list
   end
 end
