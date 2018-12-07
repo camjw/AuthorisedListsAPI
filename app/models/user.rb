9b2234324d7b77dcf002c0c708b78861c609ea56
camjw <camjw119@gmail.com>
2018-12-07 14:09:44 +0000

    updated models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@
 
 # User record
 class User < ApplicationRecord
+  has_secure_password
   has_many :wish_lists
 
-  validates_presence_of :name, :email
+  validates_presence_of :name, :email, :password_digest
 end
