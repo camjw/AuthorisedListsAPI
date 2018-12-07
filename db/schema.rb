2167d3c94bc81499d36996b82e71d5ed4d509ba9
camjw <camjw119@gmail.com>
2018-12-07 13:57:22 +0000

    Added password to user schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_193802) do
+ActiveRecord::Schema.define(version: 2018_12_07_135638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2018_12_05_193802) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "wish_lists", force: :cascade do |t|
