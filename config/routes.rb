b3dccb27af8eb91c6cb3d43f161b827f3ef58ec1
camjw <camjw119@gmail.com>
2018-12-07 15:58:17 +0000

    updated routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users do
-    resources :wish_lists
-  end
   resources :wish_lists do
     resources :items
   end
   resources :products
+  post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
 end
