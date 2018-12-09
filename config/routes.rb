d325978152bf78e3678ba4f58955f0cefd32fe0f
camjw <camjw119@gmail.com>
2018-12-09 17:00:58 +0000

    Added versioning to routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :wish_lists do
-    resources :items
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :wish_lists do
+      resources :items
+    end
+    resources :products
   end
-  resources :products
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
 end
